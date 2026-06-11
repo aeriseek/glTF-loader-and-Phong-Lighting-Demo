118b03f000eadbc619d0d56b9b1b0b9e475875ac
AErisee <mondonyu@gmail.com>
2026-06-11 16:26:28 +0300

    prepare texture class

---
+++ PhongLighting/headers/Texture.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <glad/glad.h>
+#include <stb_image.h>
+
+
+class Texture
+{
+public:
+	GLuint ID;
+	const char* type;
+	GLuint unit;
+	Texture(const char* imagePath, const char* type, GLuint slot);
+
+	void Bind();
+	void Unbind();
+	void Delete();
+};
